ca104dd213118442310f2246fd583121a7eddfa8
hjanderson <83151227+hjquiner@users.noreply.github.com>
2021-05-11 11:22:13 -0700

    Basic Components

---
+++ script.js
@@ -0,0 +1,6 @@
+// components
+const exerciseType = ['Squats', 'Lunges', 'Burpies', 'Push-Ups', 'Crunches']
+const numReps = Math.floor(Math.random() * 15)
+const numSets = Math.floor(Math.random() * 10)
+
+// message creation
