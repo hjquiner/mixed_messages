d8e3315d475d471e47139f13ad950afc8f0b8f2d
hjanderson <83151227+hjquiner@users.noreply.github.com>
2021-05-11 12:40:49 -0700

    add functions and randomized rep and set

--- script.js
+++ script.js
@@ -6,22 +6,38 @@ const exerciseCategories = {
     wholeBody: ['Burpies', 'Mountain Climbers']
 }
 
-// randomization components
-const randomCore = exerciseCategories.core[Math.floor(Math.random() * exerciseCategories.core.length)]
-const randomArms = exerciseCategories.arms[Math.floor(Math.random() * exerciseCategories.arms.length)]
-const randomLegs = exerciseCategories.legs[Math.floor(Math.random() * exerciseCategories.legs.length)]
-const randomWholeBody = exerciseCategories.wholeBody[Math.floor(Math.random() * exerciseCategories.wholeBody.length)]
-const numReps = Math.ceil(Math.random() * 15)
-const numSets = Math.ceil(Math.random() * 10)
+function randomInteger(max) {
+    return Math.ceil(Math.random() * max)
+}
 
-// message selections
-const coreGenerator = randomCore + ': ' + numReps + ' reps and ' + numSets + ' sets.'
-const armsGenerator = randomArms + ': ' + numReps + ' reps and ' + numSets + ' sets.'
-const legsGenerator = randomLegs + ': ' + numReps + ' reps and ' + numSets + ' sets.'
-const wholeBodyGenerator = randomWholeBody + ': ' + numReps + ' reps and ' + numSets + ' sets.'
+function getRandomNumSet() {
+    return randomInteger(10)
+}
+
+function getRandomNumRep() {
+    return randomInteger(15)
+}
+
+function workoutGenerator() {
+    // randomization components
+    const randomCore = exerciseCategories.core[Math.floor(Math.random() * exerciseCategories.core.length)]
+    const randomArms = exerciseCategories.arms[Math.floor(Math.random() * exerciseCategories.arms.length)]
+    const randomLegs = exerciseCategories.legs[Math.floor(Math.random() * exerciseCategories.legs.length)]
+    const randomWholeBody = exerciseCategories.wholeBody[Math.floor(Math.random() * exerciseCategories.wholeBody.length)]
+    //const numReps = Math.ceil(Math.random() * 15)
+    //const numSets = Math.ceil(Math.random() * 10)
+
+    // message selections
+    const coreGenerator = randomCore + ': ' + getRandomNumRep() + ' reps and ' + getRandomNumSet() + ' sets.'
+    const armsGenerator = randomArms + ': ' + getRandomNumRep() + ' reps and ' + getRandomNumSet() + ' sets.'
+    const legsGenerator = randomLegs + ': ' + getRandomNumRep() + ' reps and ' + getRandomNumSet() + ' sets.'
+    const wholeBodyGenerator = randomWholeBody + ': ' + getRandomNumRep() + ' reps and ' + getRandomNumSet() + ' sets.'
 
 
-console.log(coreGenerator)
-console.log(armsGenerator)
-console.log(legsGenerator)
-console.log(wholeBodyGenerator)
+    console.log(coreGenerator)
+    console.log(armsGenerator)
+    console.log(legsGenerator)
+    console.log(wholeBodyGenerator)
+}
+
+workoutGenerator()
