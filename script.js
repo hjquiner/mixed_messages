535605733d0918b0b80bbcf91bcde5852ae1409c
hjanderson <83151227+hjquiner@users.noreply.github.com>
2021-05-11 12:10:38 -0700

    exercises stored in object by type

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-// components
-const exerciseType = ['Squats', 'Lunges', 'Burpies', 'Push-Ups', 'Crunches']
-const randomExercise = exerciseType[Math.floor(Math.random() * exerciseType.length)]
-const numReps = Math.floor(Math.random() * 15)
-const numSets = Math.floor(Math.random() * 10)
+// exercise object
+const exerciseCategories = {
+    core: ['Crunches', 'Reverse Crunches'],
+    arms: ['Push-Ups', 'Shoulder Taps', 'Tripcep Dips'],
+    legs: ['Squats', 'Lunges', 'High Knees'],
+    wholeBody: ['Burpies', 'Mountain Climbers']
+}
+
+// randomization components
+const randomCore = exerciseCategories.core[Math.floor(Math.random() * exerciseCategories.core.length)]
+const randomArms = exerciseCategories.arms[Math.floor(Math.random() * exerciseCategories.arms.length)]
+const randomLegs = exerciseCategories.legs[Math.floor(Math.random() * exerciseCategories.legs.length)]
+const randomWholeBody = exerciseCategories.wholeBody[Math.floor(Math.random() * exerciseCategories.wholeBody.length)]
+const numReps = Math.ceil(Math.random() * 15)
+const numSets = Math.ceil(Math.random() * 10)
 
 // message selections
-const exerciseGenerator = randomExercise + ': ' + numReps + ' reps and ' + numSets + ' sets.'
+const coreGenerator = randomCore + ': ' + numReps + ' reps and ' + numSets + ' sets.'
+const armsGenerator = randomArms + ': ' + numReps + ' reps and ' + numSets + ' sets.'
+const legsGenerator = randomLegs + ': ' + numReps + ' reps and ' + numSets + ' sets.'
+const wholeBodyGenerator = randomWholeBody + ': ' + numReps + ' reps and ' + numSets + ' sets.'
 
-console.log(exerciseGenerator)
+
+console.log(coreGenerator)
+console.log(armsGenerator)
+console.log(legsGenerator)
+console.log(wholeBodyGenerator)
