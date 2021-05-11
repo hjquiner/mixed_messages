cffca5ff9f2135d0bf1db04392273db1e76ab6e6
hjanderson <83151227+hjquiner@users.noreply.github.com>
2021-05-11 11:41:23 -0700

    add exercise generator

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // components
 const exerciseType = ['Squats', 'Lunges', 'Burpies', 'Push-Ups', 'Crunches']
+const randomExercise = exerciseType[Math.floor(Math.random() * exerciseType.length)]
 const numReps = Math.floor(Math.random() * 15)
 const numSets = Math.floor(Math.random() * 10)
 
-// message creation
+// message selections
+const exerciseGenerator = randomExercise + ': ' + numReps + ' number of reps and ' + numSets + ' number of sets'
